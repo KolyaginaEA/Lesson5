cfa438c45f61dfa852980051bd07f9591e40096e
Ekaterina <katya_usova@bk.ru>
2023-02-01 18:34:20 +0300

    HomeWork36

---
+++ HomeWork36/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace N
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            System.Console.Write("Размер массива: ");
+            int size = System.Convert.ToInt32(System.Console.ReadLine());
+            int[] array = new int[size];
+            System.Random myRandom = new System.Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRandom.Next(-100, 100);
+                System.Console.Write("{0} ", array[i]);
+            }
+            System.Console.Write("\n");
+            int Sum = 0;
+            for (int i = 1; i < array.Length; i += 2)
+            {
+                Sum += array[i];
+            }
+            System.Console.Write("Сумма: {0}", Sum);
+            System.Console.ReadKey(true);
+        }
+    }
+}
